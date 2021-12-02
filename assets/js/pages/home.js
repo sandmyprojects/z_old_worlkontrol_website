0083c2c48e98f85851e2fad7a160a22a97c58320
frankalvarezdev <frankcontacto64@gmail.com>
2021-12-02 10:55:17 -0500

    add whatsapp button

---
+++ assets/js/pages/home.js
@@ -0,0 +1,14 @@
+// whatsapp button
+tippy('#whatsapp', {
+    content: `
+    <a href="https://wa.me/51993026437?text=prueba" target="_blank" class="button is-primary is-small">
+        Numero 1
+    </a>
+    <a href="https://wa.me/51993026437?text=prueba" target="_blank" class="button is-primary is-small">
+        Numero 2
+    </a>
+    `,
+    allowHTML: true,
+    interactive: true,
+    delay: [null, 300]
+});
