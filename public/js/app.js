7c35fe8bcc921a6521c1a14c001d71d4aadda114
frankalvarezdev <frankcontacto64@gmail.com>
2021-12-04 17:30:29 -0500

    solucion de errores | selector de elemento

--- public/js/app.js
+++ public/js/app.js
@@ -163,36 +163,37 @@ class Precios {
     }
 }
 
-var typed = new Typed('#brand', {
-    strings: ["Diseños", "Banners", "Letreros", "Marketing", "Sitios web"],
-    typeSpeed: 95,
-    showCursor: true,
-    loop: true
-});
+if (document.getElementById('brand') && document.querySelector('.m1')) {
+    var typed = new Typed('#brand', {
+        strings: ["Diseños", "Banners", "Letreros", "Marketing", "Sitios web"],
+        typeSpeed: 95,
+        showCursor: true,
+        loop: true
+    });
 
 
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml1 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.ml1 .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({ loop: false })
-    .add({
-        targets: '.ml1 .letter',
-        scale: [0.3, 1],
-        opacity: [0, 1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 600,
-        delay: (el, i) => 70 * (i + 1)
-    }).add({
-        targets: '.ml1 .line',
-        scaleX: [0, 1],
-        opacity: [0.5, 1],
-        easing: "easeOutExpo",
-        duration: 700,
-        offset: '-=875',
-        delay: (el, i, l) => 80 * (l - i)
-    })
+    anime.timeline({ loop: false })
+        .add({
+            targets: '.ml1 .letter',
+            scale: [0.3, 1],
+            opacity: [0, 1],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 600,
+            delay: (el, i) => 70 * (i + 1)
+        }).add({
+            targets: '.ml1 .line',
+            scaleX: [0, 1],
+            opacity: [0.5, 1],
+            easing: "easeOutExpo",
+            duration: 700,
+            offset: '-=875',
+            delay: (el, i, l) => 80 * (l - i)
+        })
     // .add({
     //     targets: '.ml1',
     //     opacity: 0,
@@ -200,3 +201,4 @@ anime.timeline({ loop: false })
     //     easing: "easeOutExpo",
     //     delay: 1000
     // });
+}
